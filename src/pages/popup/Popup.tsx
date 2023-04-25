a08f5f462c72223443bfe822d9a072a64312da45
Clemence Goh <clemencegoh@gmail.com>
2023-04-25 08:50:33 +0800

    change to light mode, add in title input

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -41,23 +41,42 @@ export default function Popup(): JSX.Element {
   }, []);
 
   return (
-    <div className="absolute top-0 left-0 right-0 bottom-0 text-center h-full p-3 bg-gray-800">
-      <header className="flex flex-col items-center justify-center text-white">
-        <form className="bg-grey-700 shadow-md rounded px-8 pt-6 pb-8 mb-4">
-          <div className="mb-4 flex-col text-align-start">
+    <div className="absolute top-0 left-0 right-0 bottom-0 h-full p-3 dark:bg-gray-900">
+      <header className="flex flex-col">
+        <h1 className="block text-lg text-center font-bold text-gray-900 dark:text-white">
+          TIL Snippet
+        </h1>
+        <form className="p-5 bg-white dark:bg-gray-900 antialiased ">
+          <div className="mb-2">
             <label
-              className="flex justify-start text-white text-sm font-bold mb-2"
-              htmlFor="snippet"
+              htmlFor="snippet-title"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
             >
-              TIL Snippet
+              Title
+            </label>
+            <input
+              type="text"
+              id="snippet-title"
+              placeholder="Title"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="message"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Content
             </label>
             <textarea
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline h-full"
-              id="snippet"
+              id="message"
+              rows={4}
+              className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="Today I learned..."
-            />
-            <img id="preview" src={imgPreview} />
+            ></textarea>
           </div>
+
+          <img id="preview" src={imgPreview} className="mt-2" />
         </form>
       </header>
     </div>
